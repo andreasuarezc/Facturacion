b2fc2fae4b76eb526817a2ac7a3c7d8fc909a9eb
Andrea <andrea0795@gmail.com>
2021-02-12 14:09:26 -0500

    Optimización al main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -15,6 +15,44 @@ public class Main {
         electrodomesticos.add(licuadora);
         electrodomesticos.add(batidora);
         System.out.println("Los electrodomesticos vendidos tienen las siguientes características: " + electrodomesticos);
+
+        Electrodomestico nev1 = new Nevera(3,"C","Importado", 150);
+        Electrodomestico nev2 = new Nevera(4,"B","Nacional", 120);
+        electrodomesticos.add(nev1);
+        electrodomesticos.add(nev2);
+
+        Electrodomestico tv1 = new Televisor(5,"C","Importado", 45, true);
+        Electrodomestico tv2 = new Televisor(6,"B","Nacional", 55, false);
+        electrodomesticos.add(tv1);
+        electrodomesticos.add(tv2);
+
+        double totalTelevisor=0;
+        double totalNevera=0;
+        double totalElectrodomesticos=0;
+        int cantidad = 0;
+        for (Electrodomestico electrodomestico : electrodomesticos){
+            if (electrodomestico instanceof Televisor){
+                totalTelevisor+=electrodomestico.getPrecio();
+                cantidad++;
+            }else if (electrodomestico instanceof Nevera){
+                totalNevera+=electrodomestico.getPrecio();
+                cantidad++;
+            }else{
+                totalElectrodomesticos+=electrodomestico.getPrecio();
+                cantidad++;
+            }
+        }
+        System.out.println("El valor total de los televisores vendidos es: " + totalTelevisor);
+        System.out.println("El valor total de los neveras vendidos es: " + totalNevera);
+        System.out.println("El valor total de los otros electrodomesticos vendidos es: " + totalElectrodomesticos);
+        System.out.println("La cantidad total de electrodomesticos vendidos es: " + cantidad);
+
+        /*ArrayList <Electrodomestico> electrodomesticos = new ArrayList<Electrodomestico>();
+        Electrodomestico licuadora = new Electrodomestico(1,"A","Nacional");
+        Electrodomestico batidora = new Electrodomestico(2, "C","Importado");
+        electrodomesticos.add(licuadora);
+        electrodomesticos.add(batidora);
+        System.out.println("Los electrodomesticos vendidos tienen las siguientes características: " + electrodomesticos);
         double precioFinalElectrodomesticos=0;
         for (Electrodomestico electrodomestico : electrodomesticos){
             precioFinalElectrodomesticos = precioFinalElectrodomesticos+electrodomestico.getPrecio();
@@ -43,7 +81,7 @@ public class Main {
         for (Televisor televisor : televisores){
             precioFinalTelevisores = precioFinalTelevisores+televisor.getPrecio();
         }
-        System.out.println("El valor total de los televisores vendidos es: " + precioFinalTelevisores);
+        System.out.println("El valor total de los televisores vendidos es: " + precioFinalTelevisores);*/
 
 
 
